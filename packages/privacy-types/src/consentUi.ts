11be0732d402bd72e6d75b2f6f103b6c15aa1d6e
linh-transcend <linh@transcend.io>
2026-06-12 14:13:06 -0400

    add back themeConfigMap

--- packages/privacy-types/src/consentUi.ts
+++ packages/privacy-types/src/consentUi.ts
@@ -90,6 +90,7 @@ export const LoadOptions = t.intersection([
     regimeAutoPromptMap: t.record(RegimeKey, t.boolean),
     variantConfigMap: t.record(VariantKey, UIConfiguration),
     variantThemeMap: t.record(VariantKey, ThemeKey),
+    themeConfigMap: t.record(ThemeKey, ThemeConfiguration),
     autofocus: AutofocusValues,
     uiZIndex: IntegerString,
     // If messageMap is not defined, messages will be fetched from `${messageFolder}/${localeKey}.json`
